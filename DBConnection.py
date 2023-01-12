458c83359cda3482a6bfdf37c9420d8f647c76ce
Saraswathi560 <122016669+Saraswathi560@users.noreply.github.com>
2023-01-12 12:47:48 +0530

    Add files via upload

---
+++ DBConnection.py
@@ -0,0 +1,8 @@
+import mysql.connector
+class DBConnection:
+    @staticmethod
+    def getConnection():
+        database = mysql.connector.connect(host="localhost", user="root", passwd="root", port='3306',db='stroke')
+        return database
+if __name__=="__main__":
+    print(DBConnection.getConnection())
